3ad32e13da1fa583edd6d77df0a02bf87ed1d6e5
syed zunair gillani <gillanix007@gmail.com>
2023-02-01 16:08:01 +0500

    final cv template page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Container = ({children}) => {
   return (
     <div className='md:flex'>
-      <div className='md:w-[60%] px-4 md:px-7 lg:px-12'> {children[0]}</div>
-      <div className='md:w-[40%] px-4 md:px-7 lg:px-10 py-9 bg-[#E3E3E3]'> {children[1]}</div>
+      <div className='md:w-[56%] px-4 md:px-7 lg:px-12'> {children[0]}</div>
+      <div className='md:w-[44%] px-4 md:px-8 lg:px-16 py-9 bg-[#E3E3E3]'> {children[1]}</div>
     </div>
   )
 }
