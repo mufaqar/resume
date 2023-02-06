63ede125d59bce59adcb2fdad53e2dad8464c93e
syed zunair gillani <gillanix007@gmail.com>
2023-02-06 13:44:52 +0500

    update placeholder

--- src/modules/Only-coverLetter-module.jsx
+++ src/modules/Only-coverLetter-module.jsx
@@ -32,7 +32,7 @@ const OnlyCoverLetterModule = () => {
                       type="text"
                       id="first_name"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Enter your first name"
+                      placeholder="Enter your name"
                       required
                     />
                   </div>
@@ -47,7 +47,7 @@ const OnlyCoverLetterModule = () => {
                       type="tel"
                       id="phone"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Enter your last name"
+                      placeholder="Enter your phone number"
                       pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                       required
                     />
@@ -118,7 +118,7 @@ const OnlyCoverLetterModule = () => {
                       type="text"
                       id="Country"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Project name"
+                      placeholder="Country name"
                       required
                     />
                   </div>
